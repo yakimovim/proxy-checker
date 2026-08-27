fb2cc0d411ffd83b224ca2a80bd40b8b6c81bf7b
Ivan Yakimov <edlinsoft@gmail.com>
2026-08-27 15:40:59 +0300

    Update chocolatey package

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/yakimovim/proxy-checker/releases/download/v1.0.2/ProxyChecker.1.0.2.win-x64.zip'
+$url = 'https://github.com/yakimovim/proxy-checker/releases/download/v1.0.3/ProxyChecker.1.0.3.win-x64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   
-  checksum      = 'A4CCE5E71F8D2553D4A32E8B0A5B5F44C7F2BADA129E739C29D64E9865AEF7BB'
+  checksum      = '1A4255CD64B2DDD212BB9BDE4E8A532B672828FDC026B27F68B685428EA8D828'
   checksumType  = 'sha256'
 }
 
